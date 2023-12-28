69bb9160e8a373ffdf489572c976f0e79cd901d4
Govind Kumar Yadav <robertgovind@gmail.com>
2023-12-28 13:36:07 +0545

    modified

--- DSA/Lab01/07_circular_queue.cpp
+++ DSA/Lab01/07_circular_queue.cpp
@@ -3,49 +3,72 @@ using namespace std;
 #define maxSize 5
 
 int Queue[maxSize];
-int front =0,rear=-1;
+int front =-1,rear=-1;
 
 void Enqueue(int data){  //enqueue
-    if(rear==maxSize-1){
+    if((front==0 && rear==maxSize-1)||front==rear+1){
         cout<<"Queue overflow!!!"<<endl;
     }
     else{
-            rear+=1;
+            if(front==-1){
+                front=rear=0;
+            }
+            else{
+                rear=(rear+1)%maxSize;
+            }
             Queue[rear]=data;
         }
         }
 
 
 void Dequeue(){  //dequeue
-    if(front>rear){
+    if(front==-1){
         cout<<"Queue underflow!!!"<<endl;
         return;
-}
+    }
     else{
-        cout<<"Element deleted from queue :"<<Queue[front]<<endl;
-        front+=1;
+        if(front==rear){
+            front=rear=-1;
+        }else{
+         cout<<"Element deleted from queue :"<<Queue[front]<<endl;
+         front=(front+1)%maxSize;   
+        }
     }
     
 }
 
 void printQueueStatus(){
+    
     for(int i=front;i<=rear;i++){
         cout<<Queue[i]<<" ";
     }
+    int i=front;
+    if(front>rear){
+    do{
+        cout<<Queue[i]<<" ";
+        i=(i+1)%maxSize;
+    }while(i!=front);
+    }
     cout<<endl;
+    cout<<"Front : "<<front<<"  Rear : "<<rear<<endl;
 }
 
 int main(){
+   
+    Enqueue(5);
+    Enqueue(2);
+    printQueueStatus();
     Dequeue();
-    Enqueue(4);
-    Dequeue();
-    // Enqueue(5);
-    // Enqueue(4);
-    // Enqueue(9);
-    // Enqueue(8);
-    // Enqueue(2);
-    // printQueueStatus();
-    // Dequeue();
-    // printQueueStatus();
+    printQueueStatus();
+    Enqueue(5);
+    printQueueStatus();
+    Enqueue(8);
+    printQueueStatus();
+    Enqueue(3);
+    printQueueStatus();
+    Enqueue(1);
+    printQueueStatus();
+   // Enqueue(5);
+    //printQueueStatus();
     return 0;
 }
