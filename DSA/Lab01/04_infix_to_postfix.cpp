0a06528251e38b562c78d45bac942ea708c071e2
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-03 21:07:13 +0545

    adde program

--- DSA/Lab01/04_infix_to_postfix.cpp
+++ DSA/Lab01/04_infix_to_postfix.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int Precedence(char ch){
+    if(ch=='^'){
+        return 2;
+    }
+    else if(ch=='*'|| ch=='/'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 void convert(char infix[],char postfix[]){
 
 }
@@ -9,7 +20,8 @@ int main(){
     char postfix[25];
     cout<<"Enter infix expression :"<<endl;
     cin>>infix;
-    cout<<endl<<infix;
+    cout<<infix<<endl;
+    cout<<Precedence('-');
 
     convert(infix,postfix);
     return 0;
