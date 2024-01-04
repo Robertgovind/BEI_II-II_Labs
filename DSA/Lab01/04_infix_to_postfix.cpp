98704fa0278d779606e855a2adabeac9781173fc
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-04 14:11:09 +0545

    added dsa lab problem

--- DSA/Lab01/04_infix_to_postfix.cpp
+++ DSA/Lab01/04_infix_to_postfix.cpp
@@ -1,28 +1,107 @@
 #include<iostream>
 using namespace std;
+#define stackSize 25
+
+char stack[stackSize];
+
+int tos=-1;
 
 int Precedence(char ch){
-    if(ch=='^'){
+    if(ch=='^' || ch=='$'){
+        return 3;
+    }
+    else if(ch=='*'|| ch=='/' || ch=='%'){
         return 2;
     }
-    else if(ch=='*'|| ch=='/'){
+    else if(ch=='+' || ch=='-'){
         return 1;
     }
     else{
-        return 0;
+        return -1;
     }
 }
-void convert(char infix[],char postfix[]){
 
+void push(char item){
+    if(tos==(stackSize-1)){
+        cout<<"Stack overflow!!"<<endl;
+    }
+    else{
+        tos+=1;
+        stack[tos]=item;
+    }
+}
+
+char pop(){
+    char data;
+    if(tos==-1){
+        cout<<"Stack Underflow!!!"<<endl;
+    }
+    else{
+        data=stack[tos];
+        tos-=1;
+    }
+    return data;
+}
+
+void printStackStatus(){
+    for(int i=tos;i>=0;i--){
+        cout<<stack[i]<<" "<<endl;
+    }
+}
+
+bool isOperator(char ch){
+    if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^' || ch=='(' || ch==')'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void convert(string infix,string postfix){
+    for(int i=0;i<infix.length();i++){
+        char ch=infix[i];
+        if(isOperator(ch)){
+            if(ch=='('){
+                push(ch);
+            }
+            else if(ch==')'){
+                while(ch!='('){
+                    postfix+=pop();
+                }
+                pop();
+            }
+            else{
+                if(tos==-1){
+                    push(ch);
+                }
+                else if(Precedence(stack[tos]>=Precedence(ch))){
+                    postfix+=pop();
+                    push(ch);
+                }
+                else{
+                    push(ch);
+                }
+            }
+        }
+        else{
+            postfix+=ch;
+        }
+     
+    }
+    while (tos!=-1){
+        postfix+=pop();
+       }
+    cout<<postfix<<endl;
+
+    
 }
 int main(){
-    char infix[25];
-    char postfix[25];
+    string infix;
+    string postfix="";
     cout<<"Enter infix expression :"<<endl;
     cin>>infix;
-    cout<<infix<<endl;
-    cout<<Precedence('-');
-
     convert(infix,postfix);
+    cout<<postfix;
     return 0;
 }
