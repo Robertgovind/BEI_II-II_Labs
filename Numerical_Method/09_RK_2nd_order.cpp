2d59103837493bfbcabefdef494a30c59d23ca36
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-18 09:22:14 +0545

    added rk 2nd order method

--- Numerical_Method/09_RK_2nd_order.cpp
+++ Numerical_Method/09_RK_2nd_order.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 #include<bits/stdc++.h>
-
 using namespace std;
 
 double F(double x,double y){
     return (x+pow(y,0.5));
 }
+
 int main(){
+    double x0,y0,x,y,h,xn,m1,m2;
+    cout<<"Enter x0, y0, h, xn :"<<endl;
+    cin>>x0>>y0>>h>>xn;
+    x=x0,y=y0;
+    while(x!=xn){
+        m1=F(x,y);
+        x=x+h;
+        m2=F(x,y+h*m1);
+        y=y+h*(m1+m2);
+
+    }
     return 0;
 }
